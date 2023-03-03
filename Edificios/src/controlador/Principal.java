d3dc3934d4ce700da32e8da57171497bff914f54
Daniel Ibañez <daniel.im97@gmail.com>
2023-03-03 22:05:59 +0100

    Tercer commit

---
+++ Edificios/src/controlador/Principal.java
@@ -0,0 +1,18 @@
+
+package controlador;
+
+import consola.Consola;
+
+
+/**
+ *
+ * @author soxcr
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+          Consola consola = new Consola();
+          //Llamamos a menu
+          consola.menu();
+    } 
+}
